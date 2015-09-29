49a70b09990de477e65e39b1d2fdc1a2c0716916
Khriz Enríquez <christoferen7@gmail.com>
2015-09-29 00:12:54 -0600

    Agregando clases para que login se vea mas decente, esta en etapa experimental aún, solo se agregaron las librerías de material design + bootstrap y se esta jugando aún con lo responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,39 @@
 @extends('layouts.layout')
-@section('css')
+@section('styles')
 
-	<title>Título</title>
+	<title>Inicia sesión</title>
 
 @stop
 @section('content')
 <body>
 	<h1>Login</h1>
 
-	<form action="/">
-		<input type="text" placeholder="Usuario" />
-		<input type="password" placeholder="Contraseña" />
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 col-md-4 col-md-offset-4">
+				<form action="/" class="form-horizontal">
+					<fieldset>
+						 <div class="form-group">
+				            <!-- <label for="email" class="col-lg-3 control-label">Email</label> -->
+				            <div class="col-xs-12">
+				                <input type="email" class="form-control" id="email" name="email" placeholder="Email" maxlength="99" />
+				            </div>
+				        </div>
+				        <div class="form-group">
+				            <!-- <label for="password" class="col-lg-2 control-label">Contraseña</label> -->
+				            <div class="col-xs-12 col-md-8">
+				                <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña" maxlength="99" />
+				            </div>
+				            <div class="col-xs-6 col-md-4">
+								<button class="btn btn-primary btn-raised" type="button">Envíar</button>
+				            </div>
+				        </div>
 
-		<button class="btn btn-primary btn-raised" type="button">Envíar</button>
-	</form>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
 @stop
 @section('scripts')
 @stop
