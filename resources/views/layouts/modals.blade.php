19f77689312fb14a061771ec437b020e6807006a
Khriz Enríquez <christoferen7@gmail.com>
2015-10-01 23:33:50 -0600

    Agregando archivo donde estarán las modales generales

---
+++ resources/views/layouts/modals.blade.php
@@ -0,0 +1,13 @@
+<!-- General modal -->
+
+<div id="generalModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog">
+  	<div class="modal-dialog modal-sm">
+    	<div class="modal-content">
+      		<h3>Title</h3>
+      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis culpa tempore qui dolorem, error reiciendis dolorum animi eligendi, quos nesciunt ut at nihil quod deserunt quam ad, nemo fugiat eius!</p>
+    	</div>
+  	</div>
+</div>
+
+<!-- Error modal -->
+<!-- Success modal -->
