dd45a16c74112243c591f479c6ab92a82b965803
Khriz Enríquez <christoferen7@gmail.com>
2015-10-06 22:24:43 -0600

    Estilos de la vista de catalogos

--- resources/views/catalogs.blade.php
+++ resources/views/catalogs.blade.php
@@ -35,8 +35,78 @@
 				    <div class="panel-heading">
 				        <h3 class="panel-title">Panel primary</h3>
 				    </div>
+
+					<form action="" method="post" class="form-horizontal panel-body">
+						<fieldset>
+							<div class="form-group">
+					            <div class="col-xs-12 col-md-4">
+					                <input type="text" class="form-control" id="search" placeholder="Buscar" />
+					            </div>
+					        </div>
+
+					        <div class="form-group">
+					        	<div class="col-xs-12 col-md-8">
+					        		<button type="button" class="btn btn-success btn-raised">Crear</button>
+					        	</div>
+					        </div>
+						</fieldset>
+					</form>
+
 				    <div class="panel-body">
-				        Panel content
+				        <table class="table table-striped table-hover">
+						    <thead>
+						        <tr>
+						            <th>#</th>
+						            <th>Column heading</th>
+						            <th>Column heading</th>
+						            <th>Column heading</th>
+						        </tr>
+						    </thead>
+						    <tbody>
+						        <tr>
+						            <td>1</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						        <tr>
+						            <td>2</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						        <tr class="info">
+						            <td>3</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						        <tr class="success">
+						            <td>4</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						        <tr class="danger">
+						            <td>5</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						        <tr class="warning">
+						            <td>6</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						        <tr class="active">
+						            <td>7</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						            <td>Column content</td>
+						        </tr>
+						    </tbody>
+						</table>
 				    </div>
 				</div>
 			</div>
