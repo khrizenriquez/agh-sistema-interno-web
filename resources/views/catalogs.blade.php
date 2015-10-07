75e5152290b0bb1bf60e53371341b5d8865322a4
Khriz Enríquez <christoferen7@gmail.com>
2015-10-06 21:59:55 -0600

    Estilos de la vista de catalogos

--- resources/views/catalogs.blade.php
+++ resources/views/catalogs.blade.php
@@ -1,9 +1,50 @@
-<div>
-	Catalogos
-</div>
+@if(!App\Models\User::isLogged())
+<?php redirect('/inicio'); ?>
+@endif
 
-<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quos, sunt velit natus magnam quae incidunt odit enim ea ad neque nulla doloribus repudiandae nostrum totam veniam quibusdam, quia dicta.</div>
-<div>Doloribus fugit nostrum deserunt, aliquam sint perspiciatis aspernatur, provident sapiente numquam nulla non nisi quia aperiam, incidunt autem! Incidunt nobis in reiciendis dolores ut, sed corporis beatae architecto doloremque est.</div>
-<div>Incidunt repudiandae assumenda facilis sint ratione, adipisci dolore, est praesentium quae quibusdam ipsam cupiditate fugiat consequuntur dignissimos! Perferendis, ab? Sed iste, error rerum enim pariatur amet nulla molestias doloremque optio.</div>
-<div>Velit deserunt adipisci autem, at. Dignissimos reiciendis debitis illum qui, quia, est consequatur. Fuga nemo doloribus repellat, dolorem iure harum quo ea molestiae et inventore voluptas ducimus, dolorum maxime, perspiciatis.</div>
-<div>Ab praesentium tempore quae, deleniti, nostrum magni dicta iste rem facere fugit pariatur sapiente quia delectus architecto quo minus dolore iure explicabo illo, omnis nulla voluptates est aliquid laudantium? Consequatur.</div>
+@extends('layouts.layout')
+@section('styles')
+
+	<title>Catalogos</title>
+
+@stop
+@section('content')
+<body>
+	<h1>Catálogos</h1>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-2">
+				<ul class="nav nav-pills nav-stacked">
+				  	<li role="presentation" class="active">
+				  		<a href="#">Departamentos</a>
+				  	</li>
+				  	<li role="presentation">
+				  		<a href="#">Municipios</a>
+				  	</li>
+				  	<li role="presentation">
+				  		<a href="#">Tipos de responsable</a>
+				  	</li>
+				  	<li role="presentation">
+				  		<a href="#">Tipos de enfermedades</a>
+				  	</li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-md-10">
+				<div class="panel panel-primary">
+				    <div class="panel-heading">
+				        <h3 class="panel-title">Panel primary</h3>
+				    </div>
+				    <div class="panel-body">
+				        Panel content
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
+
+@section('scripts')
+<script src="/js/protos/login.js"></script>
+<script src="/js/login.js"></script>
+@stop
