a2afd7463fd7fa369e4afde9e7645588ae28dd49
Khriz Enríquez <christoferen7@gmail.com>
2015-10-12 18:25:10 -0600

    Agregando modal general para los catalogos

--- resources/views/catalogs.blade.php
+++ resources/views/catalogs.blade.php
@@ -35,6 +35,28 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Option modal -->
+	<div id="catalogsModal" class="modal fade" tabindex="-1" role="dialog" data-tb-id>
+	  	<div class="modal-dialog">
+			<div class="modal-content">
+		      	<div class="modal-header">
+		        	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        	<h3 class="modal-title">¿Qué deseas hacer?</h3>
+		      	</div>
+
+		      	<div class="modal-body">
+		      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic at commodi in tempore perferendis sequi ullam repudiandae dolores porro, quos provident velit consectetur dolore quibusdam asperiores, fugiat tempora possimus. Illum.</p>
+		      	</div>
+
+		      	<div class="modal-footer">
+		        	<button type="button" class="btn btn-info" data-dismiss="modal">Ver información</button>
+		        	<button type="button" class="btn btn-default" data-dismiss="modal">Editar</button>
+		        	<button type="button" class="btn btn-danger" data-dismiss="modal">Eliminar</button>
+		      	</div>
+		    </div>
+	  	</div>
+	</div>
 @stop
 
 @section('scripts')
