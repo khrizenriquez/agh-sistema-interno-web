7b119d8f157154b68d27a59ddb082101132e3427
Khriz Enríquez <christoferen7@gmail.com>
2015-10-13 21:49:21 -0600

    Archivos y acciones para mostrar catalogos

--- public/js/login.js
+++ public/js/login.js
@@ -32,6 +32,19 @@ $(function () {
 		return;
 	}
 });
+
+var vague = $('.container-back').Vague({
+    intensity:      10,      // Blur Intensity
+    forceSVGUrl:    false,   // Force absolute path to the SVG filter,
+    // default animation options
+    animationOptions: {
+      duration: 1000,
+      easing: 'linear' // here you can use also custom jQuery easing functions
+    }
+});
+
+vague.blur();
+
 function logout () {
 	var u = new User();
 	u.logout(function () {
