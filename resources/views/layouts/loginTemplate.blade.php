9871fa1300a1d59c8d2feb8af0efe6b308dfe520
Khriz Enríquez <christoferen7@gmail.com>
2015-10-13 21:45:53 -0600

    Cambiando el layout para el login

---
+++ resources/views/layouts/loginTemplate.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="/styles/idealform/jquery.idealforms.css" />
+    <!-- Twitter Bootstrap -->
+    <link rel="stylesheet" href="/styles/bootstrap/bootstrap.min.css" />
+
+    <!-- Material Design for Bootstrap -->
+    <link rel="stylesheet" href="/styles/roboto.css" />
+    <link rel="stylesheet" href="/styles/bootstrap/material/material-fullpalette.css" />
+    <link rel="stylesheet" href="/styles/bootstrap/material/ripples.css" />
+
+    <!-- Dropdown.js -->
+    <!-- <link rel="stylesheet" href="//cdn.rawgit.com/FezVrasta/dropdown.js/master/jquery.dropdown.css" /> -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+    <link rel="stylesheet" href="/styles/home.css" />
+    @yield('styles')
+    <script src="/js/modernizr.js"></script>
+  </head>
+
+  <body>
+
+  @include('layouts.generalModals')
+
+  @yield('content')
+  	<!-- Footer -->
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+      <div class="container">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum alias suscipit autem, magnam voluptate libero, labore amet, odio repellat corporis sint ducimus dolorum, facere temporibus fuga delectus ad! Facilis, provident.
+      </div>
+    </nav>
+
+  <script src="/js/vendor/jquery.js"></script>
+
+  <!-- Twitter Bootstrap -->
+  <script src="/js/vendor/bootstrap/bootstrap.min.js"></script>
+
+  <!-- Material Design for Bootstrap -->
+  <script src="/js/vendor/bootstrap/material/material.js"></script>
+  <script src="/js/vendor/bootstrap/material/ripples.js"></script>
+  <script>
+    $.material.init();
+  </script>
+
+  <script src="/js/vendor/idealform/jquery.idealforms.min.js"></script>
+  <script src="/js/vendor/idealform/jquery.idealforms.i18n.es.js"></script>
+  <script src="/js/main.js"></script>
+  <script src="/js/login.js"></script>
+  @yield('scripts')
+  </body>
+</html>
