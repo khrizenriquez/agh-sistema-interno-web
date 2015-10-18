539a3d5b471dcbca80e1e7a122bd230429a1a5cd
Khriz Enríquez <christoferen7@gmail.com>
2015-10-18 11:14:07 -0600

    Navbar superior

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -27,10 +27,21 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="/">
             AGH
           </a>
         </div>
+
+
+        <div class="navbar-collapse collapse navbar-inverse-collapse">
+          <ul class="nav navbar-nav">
+              <li class="active"><a href="/catalogos">Catálogos</a></li>
+              <li class=""><a href="/perfil">Perfil</a></li>
+          </ul>
+        </div>
+
+
+
       </div>
     </nav>
 
