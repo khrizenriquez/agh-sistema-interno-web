db428fee6ca889eac944ec3a6a748c3ced8b4141
Khriz Enríquez <christoferen7@gmail.com>
2015-11-01 21:23:47 -0600

    Enviando los datos del usuario cuando este loggueado

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -18,38 +18,33 @@ class PageController extends Controller {
     function getPage($page) {
         $params = [];
 
+        if (User::isLogged()) {
+            $u = User::getUser();
+            $explodeName = explode(' ', $u->user_name);
+            $first  = (strlen($explodeName[0]) > 0) ? $explodeName[0]: '';
+            $last   = (isset($explodeName[1]) > 0) ? $explodeName[1]: '';
+            $params['userName']     = $u->user_name;
+            $params['userInfo']     = $u;
+            $params['firstName']    = $first;
+            $params['lastName']     = $last;
+        }
+
         switch ($page) {
             case 'inicio':
-                return (User::isLogged()) ? view('home'): view('login');
-
-                /*if (User::isLogged()) {
-                    $user = User::getUser($_SESSION['ina_user']['id']);
-                    return view('home', array('user' => $user))->with($params);
-                } else {
-                    return view('home')->with($params);
-                }*/
+                return (User::isLogged()) ? view('home')->with($params) : view('login');
             break;
             case 'perfil':
-                if (!User::isLogged()) {
-                    return redirect('/inicio');
-                }
-                return print var_dump(User::getUser());
+                return (User::isLogged()) ? view('profile')->with($params) : redirect('/inicio');
                 /*$user = User::getUser($_SESSION['ina_user']['id']);
                 $user_ext = User::join("state","state.id","=","user.state_id")->join("country","country.id","=","state.country_id")->where("user.id","=",$_SESSION['ina_user']['id'])->get(array("country.phone_ext"))->first();
                 $params['page']['ext']=$user_ext->phone_ext;
                 return view('profile', array('user' => $user))->with($params);*/
             break;
             case 'catalogos':
-                if (!User::isLogged()) {
-                    return redirect('/inicio');
-                }
-                return view('catalogs');
+                return (User::isLogged()) ? view('catalogs')->with($params) : redirect('/inicio');
             break;
             case 'pacientes':
-                if (!User::isLogged()) {
-                    return redirect('/inicio');
-                }
-                return print var_dump('pacientes');
+                return (User::isLogged()) ? view('patients')->with($params) : redirect('/inicio');
             break;
             default: 
             break;
