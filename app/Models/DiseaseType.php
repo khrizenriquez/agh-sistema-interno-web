8c20b99a5fd403d1da70d8fa9452ae3cc025c3d6
Khriz Enríquez <christoferen7@gmail.com>
2015-11-22 17:25:40 -0600

    FUnciones para crud de enfermedades

--- app/Models/DiseaseType.php
+++ app/Models/DiseaseType.php
@@ -5,18 +5,56 @@ use Illuminate\Database\Eloquent\Model;
 
 class DiseaseType extends Model {
 
-    protected $table        = 'disease_type';
-    protected $timestamp    = false;
+    protected $table    = 'disease_type';
+    public $timestamps  = false;
 
     public static function getTotal () {
-        return DiseaseType::all()->count();
+        return static::all()->count();
     }
 
-    public static function getAllDiseases () {
-        $model = DiseaseType::where('status', '=', 1)
-                            ->orderBy('id', 'asc')
+    public static function createDiseaseType ($name, $status, $userId) {
+        $disease = new DiseaseType();
+
+        $date = date('Y-m-d H:i:s');
+
+        $disease->name                  = $name;
+        $disease->status                = $status;
+        $disease->created_at            = $date;
+        $disease->updated_at            = $date;
+        $disease->fk_user_who_create    = $userId;
+
+        $disease->save();
+
+        return $disease;
+    }
+
+    public static function getAllDiseaseType () {
+        $model = static::orderBy('id', 'asc')
                             ->get(['created_at', 'updated_at', 'name', 'id', 'status']);
         return $model;
+    }
+
+    public static function deleteDiseaseType ($id) {
+        $disease = static::find($id);
+
+        $disease->status     = 0;
+        $disease->updated_at = date('Y-m-d H:i:s');
+
+        $disease->update();
+
+        return $disease;
+    }
+
+    public static function updateDisease ($id, $name, $status) {
+        $disease = static::find($id);
+
+        $disease->name          = $name;
+        $disease->status        = $status;
+        $disease->updated_at    = date('Y-m-d H:i:s');
+
+        $disease->update();
+
+        return $disease;
     }
 
 }
